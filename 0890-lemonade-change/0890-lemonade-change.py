b765e31fa568fae41a4b613674e0b820839cae50
PAVAN DANIEL <132094389+gurrampavan55@users.noreply.github.com>
2025-08-04 23:40:14 +0530

    Time: 4 ms (62.08%), Space: 15.5 MB (23.79%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.py
+++ 0890-lemonade-change/0890-lemonade-change.py
@@ -4,30 +4,24 @@ class Solution(object):
         :type bills: List[int]
         :rtype: bool
         """
-        tens=0
         fives=0
-        for num in bills:
-            if num==5:
+        tens=0
+        for bill in bills:
+            if bill==5:
                 fives+=1
-            elif num==10:
+            elif bill==10:
                 if fives==0:
                     return False
-                fives-=1
                 tens+=1
+                fives-=1
             else:
-                if tens>0 and fives>0:
-                    tens-=1
-                    fives-=1
-                elif fives>=3:
-                    fives-=3
+                if tens > 0 and fives > 0:
+                    tens -= 1
+                    fives -= 1
+                elif fives >= 3:
+                    fives -= 3
                 else:
                     return False
         return True
-
-                
-
-            
-            
-
 
         
