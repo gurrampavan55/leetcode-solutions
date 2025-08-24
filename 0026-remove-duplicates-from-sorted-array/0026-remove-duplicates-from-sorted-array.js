0b1e7456060289a6bc4c62089624f334599bca01
PAVAN DANIEL <132094389+gurrampavan55@users.noreply.github.com>
2025-08-24 19:06:23 +0530

    Time: 0 ms (100%), Space: 58.3 MB (19.84%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+     let j=0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]>nums[j]){
+            j=j+1;
+            nums[j]=nums[i]
+        }
+        
+    }
+    return j+1
+    
+};
