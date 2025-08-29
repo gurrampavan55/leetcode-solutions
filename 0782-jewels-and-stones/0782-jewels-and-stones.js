a8c2bc563e43f5e359afea630d0782e5561d4eb0
PAVAN DANIEL <132094389+gurrampavan55@users.noreply.github.com>
2025-08-29 08:56:33 +0530

    Time: 1 ms (49.81%), Space: 54.4 MB (26.77%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.js
+++ 0782-jewels-and-stones/0782-jewels-and-stones.js
@@ -5,16 +5,11 @@
  */
 var numJewelsInStones = function(jewels, stones) {
     let count=0;
-    for(let i=0;i<stones.length;i++){
-        for(let j=0;j<jewels.length;j++){
-            if(jewels[j]===stones[i]){
-                count++;
-                break
-            }
-        
-    }
+    let Jset=new Set(jewels);
+    for (let char of stones){
+        if(Jset.has(char)){
+           count++
+        }
     }
     return count
-    
-    
 };
