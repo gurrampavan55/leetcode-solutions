8297c1d29cf7f3fde1958e83d9e06278a5e8c7e9
PAVAN DANIEL <132094389+gurrampavan55@users.noreply.github.com>
2025-08-29 10:00:43 +0530

    Time: 13 ms (13.05%), Space: 58.3 MB (16.81%) - LeetHub

---
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxFreqSum = function(s) {
+    let map=new Map();
+    for(let char of s){
+        if(!map[char]){
+            map[char]=1;
+        }
+        else{
+            map[char]++;
+        }
+    }
+    let vowels=['a','e','i','o','u'];
+    let max_vowel=0;
+    let max_consonant=0;
+    for(let char of s){
+        if(vowels.includes(char)){
+           max_vowel=Math.max(map[char],max_vowel)
+        }
+        else{
+            max_consonant=Math.max(map[char],max_consonant)
+        }
+    }
+    return max_vowel+max_consonant
+};
