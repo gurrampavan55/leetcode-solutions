694d008083c5f696754f91e1b0979707e28b9caf
PAVAN DANIEL <132094389+gurrampavan55@users.noreply.github.com>
2025-08-30 16:52:16 +0530

    Time: 0 ms (100%), Space: 57.4 MB (89.03%) - LeetHub

---
+++ 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} num
+ * @return {string}
+ */
+var largestOddNumber = function(num) {
+    for(i=num.length-1;i>=0;i--){
+        if(parseInt(num[i])%2===1){
+            return num.slice(0,i+1)
+        }
+
+    }
+    return ""
+    
+};
