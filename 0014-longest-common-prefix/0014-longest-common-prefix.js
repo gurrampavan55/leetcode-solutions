105914162acfb13364056453f70427e41a7293d9
PAVAN DANIEL <132094389+gurrampavan55@users.noreply.github.com>
2025-08-30 17:14:52 +0530

    Time: 1 ms (59.7%), Space: 56.2 MB (18.9%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    let x=0;
+    while(x<strs[0].length){
+        let ch=strs[0][x];
+        for(let i=0;i<strs.length;i++){
+            if(ch!==strs[i][x] || strs[i].length===x){
+                return strs[0].substring(0,x)
+
+            }
+        }
+        ++x;
+    }
+    return strs[0]
+    
+};
