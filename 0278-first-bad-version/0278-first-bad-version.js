dbfc8c171dc4116224ef011ca8157bb76a6122ec
PAVAN DANIEL <132094389+gurrampavan55@users.noreply.github.com>
2025-09-02 18:04:13 +0530

    Time: 47 ms (19.24%), Space: 53.3 MB (63.91%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.js
+++ 0278-first-bad-version/0278-first-bad-version.js
@@ -21,10 +21,10 @@ var solution = function(isBadVersion) {
         if (n===1){
             return 1
         }
-        let left=0;
+        let left=1;
         let right=n;
         while(left<=right){
-            mid=Math.floor((left+right)/2)
+          let  mid=Math.floor((left+right)/2)
              if(isBadVersion(mid) && !isBadVersion(mid-1)){
                 return mid
             }
